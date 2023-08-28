6a36b682e0176b3363f649a31f0964e10c1e9dde
olipemil <90791292+olipemil@users.noreply.github.com>
2023-08-28 14:49:43 -0400

    Update runProject.py

--- runProject.py
+++ runProject.py
@@ -4,8 +4,8 @@ from bandstructure_widget import Widget
 
 #optional input
 characterSi = ['Si s','Si pz','Si py','Si px','Si s','Si pz','Si py','Si px']
-kpathSi = [[0.5, 0.5, 0.5],[0.0, 0.0, 0.0], [0.5, 0, 0.5], [3/8,3/8,3/4] ,[0.0, 0.0, 0.0]]
-k_labelSi = [r'$L$',r'$\Gamma$',r'$X$',r'$K$',r'$\Gamma$']
+kpathSi = [[0.5, 0.5, 0.5],[0.0, 0.0, 0.0], [0.5, 0, 0.5]]#, [3/8,3/8,3/4] ,[0.0, 0.0, 0.0]]
+k_labelSi = [r'$L$',r'$\Gamma$',r'$X$']#,r'$K$',r'$\Gamma$']
 
 #required input
 wanDirect = "silicon_interact"
